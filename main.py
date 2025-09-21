5a3b86bc700b13db2603bd49fedb607270aad6b4
Panyapatkia <68130589@dpu.ac.th>
2025-09-21 21:45:05 +0700

    send

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import importlib
 from mylib import myfunc
 
-if __name__ == "__main__":
-    character = input("please input character: ")  # รับตัวอักษรจากผู้ใช้
-    round = int(input(" How many turns you want to run: "))  # รับจำนวนรอบ
+character = input("please input character: ")  # รับตัวอักษรจากผู้ใช้
+rounds = int(input("How many turns you want to run: "))  # รับจำนวนรอบ
 
-    for i in range(1, round + 1):
-        print(myfunc(character, i))
+for i in range(1, rounds + 1):
+    print(myfunc(character, i))
